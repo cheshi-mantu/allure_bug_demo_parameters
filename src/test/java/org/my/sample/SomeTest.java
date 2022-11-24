717ee4ab6cff73013d19575e1ec056c9cccba6e0
Egor Ivanov <cheshi.mantu@pm.me>
2022-11-24 13:19:50 +0300

    see the comment in SomeTest.java

--- src/test/java/org/my/sample/SomeTest.java
+++ src/test/java/org/my/sample/SomeTest.java
@@ -1,5 +1,6 @@
 package org.my.sample;
 
+import io.qameta.allure.Allure;
 import io.qameta.allure.Param;
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.Test;
@@ -8,12 +9,17 @@ class SomeTest {
 
     @Test
     void someTest() {
+
         someParameterizedMethod("ABCD", 1);
     }
 
     @Step("Here I to something")
     void someParameterizedMethod(@Param("With string parameter") String param1,
                                  @Param("With int parameter") int param2) {
+        // for parameter to appear on Test's level you need to use Allure.parameter(), otherwise your parameter will be only the parameters of a step
+        Allure.parameter("param1", param1);
+        Allure.parameter("param2", param2);
+
 
     }
 }
